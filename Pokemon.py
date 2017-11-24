0cbd0c3d3d2c417f3eef28dc53d62518e538ee80
reefman001 <brian.kilburn2@yahoo.com>
2017-11-24 09:56:18 -0500

    Update Pokemon.py
    implemented flip and pokegenerate function, I think at this point we need to create an algorithm to have our Pokemon to generate in random positions on the screen and have them placed in logical areas, we might assign pre-defined coordinates that will do that.

--- Pokemon.py
+++ Pokemon.py
@@ -1,117 +1,273 @@
 import pygame
-import pokebase
+
+import pokebase as pb
+
 import random, math, sys
 
+
+
 #exit the program
+
 def events():
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+
             pygame.quit()
+
             sys.exit()
 
+#pokemon generating method, returns pokemon image and name   
+def _createPokemon_(dbValue):
+    sprite = pygame.image.load(pb.pokemon_sprite(dbValue).path)
+    return sprite, pb.pokemon(dbValue)
+
+#flip function
+def flipImage(image):
+    flipped = pygame.transform.flip(image, True, False)
+    return flipped
+
+
 #defining the display surface
+
 screenWidth = 800
+
 screenHeight = 600
+
 ######################
+
 halfScreenWidth = screenWidth/2
+
 halfScreenHeight = screenHeight/2
+
 #################################
+
 SCREEN_AREA = screenWidth * screenHeight
+
 ########################################
 
+
+################character creation ############
+#store pokemon ID's for api database access
+pokemon_Id = [random.randrange(1, 152),random.randrange(1, 152),random.randrange(1, 152)]
+
+#create characters generation 1 pokemon
+poke1, pokename1 = _createPokemon_(pokemon_Id[0])
+poke2, pokename2 = _createPokemon_(pokemon_Id[1])
+poke3, pokename3 = _createPokemon_(pokemon_Id[2])
+
+#middle pokemon, (flipped)left, right: display names 
+print("poke1:",pokename1)
+print("poke2:",pokename2)
+print("poke3:",pokename3)
+
 #initialize the display
+
 pygame.init()
+
 CLOCK = pygame.time.Clock()
+
 SCREEN = pygame.display.set_mode((screenWidth,screenHeight))
+
 FPS = 500
 
+
+
 #Defining a list of colors in case they are needed
+
 BLACK = (0, 0, 0)
+
 WHITE = (255, 255, 255)
+
 RED = (255, 0, 0)
+
 BLUE = (0, 0, 255)
+
 GREEN = (0, 255, 0)
+
 #########################
+
 def game_loop():
+
     #setting the background image and use convert because the pixels of the image may not be the 
+
     #same as the background. Convert saves time
+
     pokemonRoute = pygame.image.load('GreatMarsh.png').convert()
+
     #finding out what the background width and background height are of the pokemon route
+
     backgroundWidth, backgroundHeight = pokemonRoute.get_rect().size
+
     #setting the width of the route will stop the player from being able to exit the bounds of the 
+
     #pokemon route and will stop the background from scrolling out of bounds of the display surface
+
     routeWidth = backgroundWidth #* 2
+    
     routePositionX = 0
+
+
 
     scrollingRoutePositionX = halfScreenWidth
 
+
+
     #this is a circle for now, but can be changed to the player later on
+
     circleRadius = 25
+
     circlePositionX = circleRadius
 
+
+
     #difining the player positions
+
     playerPositionX = circleRadius
+
     playerPositionY = 345
+
     #tells the stage what direction to move
+
     playerVelocityX = 0
+    playerVelocityY = 0
+
+
 
 
     #setting the game loop done variable to false initially
+
     done = False
+
     #main loop
+
     while not done:
+
         #calling the events function to see if the user has left the game
+
         events()
 
-        #finding out what keys have been pressed
-        KEY_PRESSED = pygame.key.get_pressed()
-        #if the user presses the right key, then move in the positive x direction
-        if KEY_PRESSED[pygame.K_RIGHT]:
-            playerVelocityX = 1
-        #if the user presses the left key, then move in the negative x direction
-        elif KEY_PRESSED[pygame.K_LEFT]:
-            playerVelocityX = -1
-        #if no key is pressed, then do not move and remain in the same position
-        else:
-            playerVelocityX = 0
 
-        playerPositionX += playerVelocityX
-        #if the playerposition in the x direction is greater than the width
-        #of the route minus the circle radius, then push the player to the 
-        #righthand edge of the screen
-        if playerPositionX > routeWidth - circleRadius:
-            playerPositionX = routeWidth - circleRadius
-        #if the playerposition in the x direction is less than the radius
-        #of the cirle, then push the player to the lefthand corner of the screen
-        if playerPositionX < circleRadius:
-            playerPositionX = circleRadius
-        #if the player position in the x direction is less than the scrollingrouteposition
-        #then the ball will be located in the center and move with the screen
-        if playerPositionX < scrollingRoutePositionX:
-            circlePositionX = playerPositionX
-        #if the playerposition is greater than the routewidth minus
-        #the scrolling route position in the x direction, then the ball will
-        #not be in the center, and it will be located at route width minus
-        #the scrolling route position plus the width of the screen
-        elif playerPositionX > routeWidth - scrollingRoutePositionX:
-            circlePositionX = playerPositionX - routeWidth + screenWidth
-        #else the circle is in the middle of the screen
+
+        #finding out what keys have been pressed
+
+        KEY_PRESSED = pygame.key.get_pressed()
+
+        #if the user presses the right key, then move in the positive x direction
+
+        if KEY_PRESSED[pygame.K_RIGHT]:
+
+            playerVelocityX = 1
+
+        #if the user presses the left key, then move in the negative x direction
+
+        elif KEY_PRESSED[pygame.K_LEFT]:
+
+            playerVelocityX = -1
+        
+        #if the user presses the up key, then move subtract y position
+        elif KEY_PRESSED[pygame.K_UP]:
+            
+            playerVelocityY = -1
+        
+        #if the user presses the down key, then move add y position
+        elif KEY_PRESSED[pygame.K_DOWN]:
+            
+            playerVelocityY = 1
+        #if no key is pressed, then do not move and remain in the same position
+
         else:
+
+            playerVelocityX = 0
+            playerVelocityY = 0
+
+        playerPositionY += playerVelocityY
+        playerPositionX += playerVelocityX
+
+        #if the playerposition in the x direction is greater than the width
+
+        #of the route minus the circle radius, then push the player to the 
+
+        #righthand edge of the screen
+
+        if playerPositionX > routeWidth - circleRadius:
+
+            playerPositionX = routeWidth - circleRadius
+        
+        elif playerPositionY > screenHeight + 5:
+            playerPositionY =  screenHeight + 5
+        
+        elif playerPositionY < 50:
+            playerPositionY = 50
+        #if the playerposition in the x direction is less than the radius
+
+        #of the cirle, then push the player to the lefthand corner of the screen
+
+        elif playerPositionX < circleRadius:
+
+            playerPositionX = circleRadius
+
+        #if the player position in the x direction is less than the scrollingrouteposition
+
+        #then the ball will be located in the center and move with the screen
+
+        elif playerPositionX < scrollingRoutePositionX:
+
+            circlePositionX = playerPositionX
+
+        #if the playerposition is greater than the routewidth minus
+
+        #the scrolling route position in the x direction, then the ball will
+
+        #not be in the center, and it will be located at route width minus
+
+        #the scrolling route position plus the width of the screen
+
+        elif playerPositionX > routeWidth - scrollingRoutePositionX:
+
+            circlePositionX = playerPositionX - routeWidth + screenWidth
+
+        #else the circle is in the middle of the screen
+
+        else:
+
             circlePositionX = scrollingRoutePositionX
+
             routePositionX += -playerVelocityX
 
+
+
         relativePositionX = routePositionX % backgroundWidth
+
         print("Route Position" + str(routePositionX))
+
         print("Background Width" + str(backgroundWidth))
+
         print("Relative Position" + str(relativePositionX))
+
         SCREEN.blit(pokemonRoute, (relativePositionX - backgroundWidth, 0))
+
         if relativePositionX < screenWidth:
+
             SCREEN.blit(pokemonRoute, (relativePositionX, 0))
 
 
+
+
+        #blit characters
+        SCREEN.blit(poke1, (screenWidth//2,screenHeight//2))
+        SCREEN.blit(flipImage(poke2), (screenWidth//2 - 100,screenHeight//2))
+        SCREEN.blit(poke3, (screenWidth//2 + 100,screenHeight//2))
+        
         pygame.draw.circle(SCREEN, WHITE, (playerPositionX, playerPositionY - circleRadius), circleRadius, 0)
 
+
+
         pygame.display.update()
+
         CLOCK.tick(FPS)
+
         SCREEN.fill(BLACK)
+
 game_loop()
